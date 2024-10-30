06518663470a75bd5597aaf752537b7b7dda2ae9
msg5311 <50459030+msg5311@users.noreply.github.com>
2024-10-30 14:46:32 -0400

    Redesigned data layout

--- NYJ_combined_stats.sql
+++ NYJ_combined_stats.sql
@@ -1,13 +1,26 @@
 -- select statement to view data:
 select *
 from a_allstats
-order by pos
-;
+order by name;
 
 -- delete and drop statements to reset data:
 delete from a_allstats where name not like '%QB%';
+
 alter table a_allstats
-drop column pos;
+drop column rush_td;
+
+-- Passing, receiving, and rushing dataset alterations:
+alter table passing_stats rename column yds to pass_yds;
+alter table passing_stats rename column td to pass_td;
+alter table passing_stats add category char(50) default ('passing');
+
+alter table receiving_stats rename column yds to rec_yds;
+alter table receiving_stats rename column td to rec_td;
+alter table receiving_stats add category char(50) default ('receiving');
+
+alter table rushing_stats rename column yds to rush_yds;
+alter table rushing_stats rename column td to rush_td;
+alter table rushing_stats add category char(50) default ('rushing');
 
 -- insert names from receiving dataset to main ste:
 insert into a_allstats (name)
@@ -19,24 +32,35 @@ insert into a_allstats (name)
 select name
 from rushing_stats;
 
- -- adds receiving cols missing from main table:
+ -- adds cols missing from main table:
 alter table a_allstats 
+add pass_yds int null,
+add pass_td int null,
+add rec_yds int null,
+add rec_td int null,
+add rush_yds int null,
+add rush_td int null,
 add yac int null,
 add rec int null;
 
--- adds rushing cols missing from main table:
-
--- Update cols that are in both table:
+-- Update passing cols:
 update a_allstats
-set 	(yds,gp,avg,lng,td,yac,yds_g,rec)
-		= (rs.yds, rs.gp, rs.avg, rs.lng, rs.td,rs.yac,rs.yds_g,rs.rec)
+set		(pass_yds,pass_td)
+		= (ps.pass_yds, ps.pass_td)
+from passing_stats ps
+where a_allstats.name = ps.name;
+
+-- Update receiving cols:
+update a_allstats
+set 	(rec_yds,gp,avg,lng,rec_td,yac,yds_g,rec)
+		= (rs.rec_yds, rs.gp, rs.avg, rs.lng, rs.rec_td,rs.yac,rs.yds_g,rs.rec)
 from receiving_stats rs
 where a_allstats.name = rs.name;
 
--- Update cols that are in both tables (rushing and main):
+-- Update rushing cols:
 update a_allstats
-set	(yds,gp,avg,lng,td,yds_g)
-	= (gg.yds,gg.gp,gg.avg,gg.lng,gg.td,gg.yds_g)
+set	(rush_yds,gp,avg,lng,rush_td,yds_g)
+	= (gg.rush_yds,gg.gp,gg.avg,gg.lng,gg.rush_td,gg.yds_g)
 from rushing_stats gg
 where a_allstats.name = gg.name;
 
@@ -48,25 +72,30 @@ update a_allstats
 set pos = right(name,2)
 ;
 
--- Delete total rows:
+-- Data cleansing:
 delete from a_allstats where name = 'Total';
+update a_allstats
+set name = left(name,length(name)-2);
+
 
 -- create table:
 create table fantasy_points_rules (
 category char(50),
-point_value decimal(10,2)
+yds_value decimal(10,2),
+td_value decimal(10,2),
+count_value decimal(10,2)
 );
 
 -- insert values into fantasy table:
-insert into fantasy_points_rules (category,point_value)
+insert into fantasy_points_rules (category,yds_value,td_value,count_value)
 values
-('rec_yds', 0.1),
-('rush_yds', 0.1),
-('pass_yds',0.04),
-('rec_td',6),
-('rush_td',6),
-('pass_td',4),
-('rec_count',0.5);
+('rec', 0.1,6,0.5),
+('rush', 0.1,6,0),
+('pass',0.04,4,0)
+;
 
 select * from fantasy_points_rules;
 
+-- add fantasy points column into main table:
+alter table a_allstats
+add column fantasy_pts decimal(10,2);
