c388769c249d6a2de57ba1d2216c7aac114abab1
msg5311 <50459030+msg5311@users.noreply.github.com>
2024-11-04 15:01:02 -0500

    NYJ - fantasy points
    add total fantasy pts column

--- NYJ_combined_stats.sql
+++ NYJ_combined_stats.sql
@@ -1,5 +1,6 @@
--- drop table a_allstats:
+-- drop tables for reset:
 drop table a_allstats;
+drop table fantasy_points_rules;
 
 -- create a_allstats table:
 create table a_allstats (
@@ -126,4 +127,37 @@ select * from fantasy_points_rules;
 
 -- add fantasy points column into main table:
 alter table a_allstats
-add column fantasy_pts decimal(10,2);
+add column fantasy_pass_pts decimal(10,2),
+add column fantasy_rush_pts decimal(10,2),
+add column fantasy_rec_pts decimal(10,2)
+;
+
+update a_allstats
+set fantasy_pass_pts = (pass_yds * fantasy_points_rules.yds_value) + (pass_td * fantasy_points_rules.td_value)
+from fantasy_points_rules
+where category = 'pass';
+
+update a_allstats
+set fantasy_rush_pts = (rush_yds * fantasy_points_rules.yds_value) + (rush_td * fantasy_points_rules.td_value)
+from fantasy_points_rules
+where category = 'rush';
+
+update a_allstats
+set fantasy_rec_pts = min((rec_yds * fantasy_points_rules.yds_value) + (rec_td * fantasy_points_rules.td_value) + (rec * fantasy_points_rules.count_value),0)
+from fantasy_points_rules
+where category = 'rec';
+
+alter table a_allstats
+add testing decimal(10,2);
+
+-- Replace nulls in fantasy pts cols with zeros...  change previous updates statements to min() in future:
+update a_allstats set fantasy_pass_pts = 0 where fantasy_pass_pts is null;
+update a_allstats set fantasy_rush_pts = 0 where fantasy_rush_pts is null;
+update a_allstats set fantasy_rec_pts = 0 where fantasy_rec_pts is null;
+
+update a_allstats
+set fantasy_pts = fantasy_pass_pts + fantasy_rush_pts + fantasy_rec_pts;
+
+select *
+from a_allstats
+order by fantasy_pts desc;
