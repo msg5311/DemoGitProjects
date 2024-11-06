47472dcde4fd817b09e065abb024f8af5e90183d
msg5311 <50459030+msg5311@users.noreply.github.com>
2024-11-06 15:24:26 -0500

    rownumber ex

--- bakery_windowfunc_ex.sql
+++ bakery_windowfunc_ex.sql
@@ -13,3 +13,26 @@ join f_customer_orders co
 	group by c.customer_id, first_name, order_total
 order by customer_id ASC
 ;
+
+-- Row number test
+select c.customer_id, 
+first_name, 
+order_total,
+row_number() over(partition by c.customer_id order by order_total ASC) as row_num -- generate a row number, over the data set, subset by customer id, and ordered by order_total in that subset.
+from e_customers c
+join f_customer_orders co
+	on c.customer_id = co.customer_id;
+
+-- use rownumber to identify the top 2 orders from each customer:
+select *
+from (
+	select c.customer_id, 
+	first_name, 
+	order_total,
+	row_number() over(partition by c.customer_id order by order_total ASC) as row_num -- generate a row number, over the data set, subset by customer id, and ordered by order_total in that subset.
+	from e_customers c
+	join f_customer_orders co
+		on c.customer_id = co.customer_id
+) as row_table -- Creating subquery
+where row_num < 3
+;
