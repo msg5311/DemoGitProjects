bcafc135116953fb3f77be9cf69bca14b76ce935
msg5311 <50459030+msg5311@users.noreply.github.com>
2024-11-06 16:22:53 -0500

    lag and lead ex

--- bakery_windowfunc_ex.sql
+++ bakery_windowfunc_ex.sql
@@ -1,51 +1,23 @@
-select *
-from e_customers;
-
--- Partition by tells sql what column to use as a subset for the function.
-select c.customer_id, 
-first_name, 
-order_total, 
-Max(order_total) 
-over (partition by c.customer_id) as max_order_total
-from e_customers c
-join f_customer_orders co
-	on c.customer_id = co.customer_id
-	group by c.customer_id, first_name, order_total
-order by customer_id ASC
-;
-
--- Row number test
-select c.customer_id, 
-first_name, 
-order_total,
-row_number() over(partition by c.customer_id order by order_total ASC) as row_num -- generate a row number, over the data set, subset by customer id, and ordered by order_total in that subset.
-from e_customers c
-join f_customer_orders co
-	on c.customer_id = co.customer_id;
-
--- use rownumber to identify the top 2 orders from each customer:
-select *
-from (
-	select c.customer_id, 
-	first_name, 
-	order_total,
-	row_number() over(partition by c.customer_id order by order_total ASC) as row_num -- generate a row number, over the data set, subset by customer id, and ordered by order_total in that subset.
-	from e_customers c
-	join f_customer_orders co
-		on c.customer_id = co.customer_id
-) as row_table -- Creating subquery
-where row_num < 3
-;
-
--- rank function needs an order by... can have duplicate ranks but will skip the next, dense rank does not skip next. 
+-- lag and lead statements:
 select *,
-rank() over (partition by department order by salary desc),
-dense_rank() over (partition by department order by salary desc)
+lag(salary) over(partition by department),
+lead(salary) over(partition by department)
 from h_employees;
 
-select *
-from(
-	select *,dense_rank() over (partition by department order by salary desc) as rank_row
-	from h_employees
-) as ranked
-where rank_row < 3;
+select *, lag_col - salary as pay_disc
+from
+(select *, -- subquery
+lag(salary) over(partition by department order by employee_id) as lag_col
+from h_employees) as lag_table;
+
+select *, 
+case
+	when salary > lag_col then 'More'
+	when salary < lag_col then 'Less'
+	else 'Even'
+	end as category_salary
+from
+(select *,
+	lead(salary) over(partition by department order by employee_id) as lag_col
+	from h_employees) as lag_table
+;
