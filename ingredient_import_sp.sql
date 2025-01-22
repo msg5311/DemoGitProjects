5ed77d4c78c169995c17332ab9bb1ebb0d3ac50c
msg5311 <50459030+msg5311@users.noreply.github.com>
2025-01-22 09:24:20 -0500

    ingredient import update
    Ingredient import sp now includes solid + liquid conversions.

--- ingredient_import_sp.sql
+++ ingredient_import_sp.sql
@@ -1,7 +1,7 @@
 
 -- Code to create a blank table to receive ingredients test:
 select * into "MEP".all_ingredients_test from "MEP".all_ingredients_purchased;
-delete from "MEP".all_ingredients_test;
+delete from "MEP".z_ingredient_import_test;
 alter table "MEP".ingredient_import_test drop column base_unit_identifier, drop column conversion, drop column unit_price;
 
 -- Begin stored procedure:
@@ -13,11 +13,29 @@ begin
 
 -- Take values from ingredient import list, join on solid_weight_conv table to convert and get to unit price, then append to all ingredients list:
 insert into "MEP".z_all_ingredients_test 
+
+with
+solid_cte as (
+
+-- Solid weight conv join:
 select z_ingredient_import_test.*,b1_solid_weight_conv.base as base_unit_identifier, b1_solid_weight_conv.conv as conversion,  round(price/(amount*b1_solid_weight_conv.conv),2) as unit_price
 from "MEP".z_ingredient_import_test
 inner join "MEP".b1_solid_weight_conv
 on z_ingredient_import_test.unit = b1_solid_weight_conv.recipe
-where b1_solid_weight_conv.base = 'oz';
+where b1_solid_weight_conv.base = 'oz'),
+
+liquid_cte as (
+-- liquid weight conv join:
+select z_ingredient_import_test.*, b2_liquid_vol_conv.base as base_unit_identifier, b2_liquid_vol_conv.conv as conversion, round(price/(amount*b2_liquid_vol_conv.conv),2) as unit_price
+from "MEP".z_ingredient_import_test
+inner join "MEP".b2_liquid_vol_conv
+on z_ingredient_import_test.unit = b2_liquid_vol_conv.recipe
+where b2_liquid_vol_conv.base = 'fl oz')
+
+-- union these two together:
+select * from solid_cte
+union
+select * from liquid_cte;
 
 commit;
 
